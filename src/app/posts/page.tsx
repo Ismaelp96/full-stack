c6f398c1ba22cc01d65b6e272b0717af5b72813d
ismaelp96 <ismael6991@live.com>
2024-05-27 17:03:11 -0300

    suspense fallback

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,21 +1,13 @@
-import Link from 'next/link';
+import PostList from '@/components/posts-list';
+import { Suspense } from 'react';
 
 const PostPage = async () => {
-  const response = await fetch('https://dummyjson.com/posts?limit=10');
-  const data = await response.json();
-
   return (
     <main className='text-center pt-32 px-5'>
       <h1 className='text-4xl md:text-5xl font-bold mb-5'>All Posts</h1>
-      <ul>
-        {data.posts.map((post: any) => (
-          <li key={post.id} className='mb-5'>
-            <Link href={`/posts/${post.id}`} className='text-xl'>
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <Suspense fallback='Loading...'>
+        <PostList />
+      </Suspense>
     </main>
   );
 };
