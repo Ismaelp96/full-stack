495c97b0ddd7c521ca1246f1d57f1b069aff4ce2
ismaelp96 <ismael6991@live.com>
2024-05-27 15:29:48 -0300

    page post

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,21 @@
-const PostPage = () => {
+import Link from 'next/link';
+
+const PostPage = async () => {
+  const response = await fetch('https://dummyjson.com/posts?limit=10');
+  const data = await response.json();
+
   return (
     <main className='text-center pt-32 px-5'>
       <h1 className='text-4xl md:text-5xl font-bold mb-5'>All Posts</h1>
+      <ul>
+        {data.posts.map((post: any) => (
+          <li key={post.id} className='mb-5'>
+            <Link href={`/posts/${post.id}`} className='text-xl'>
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
